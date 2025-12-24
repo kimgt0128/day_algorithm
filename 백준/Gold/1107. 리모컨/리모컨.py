264fa83d0a56cfbc5aaa0347a812d3dc90ba23af
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2025-12-25 00:04:55 +0900

    [Gold IV] Title: 리모컨, Time: 912 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,19 +1,26 @@
 import sys
+from itertools import product
 
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 
-broken = set(sys.stdin.readline().split()) if m > 0 else set()
+error = set(sys.stdin.readline().split()) if m > 0 else set()
+
+remain = [str(i) for i in range(10) if str(i) not in error]
 
 ans = abs(n - 100)
 
-for channel in range(1000001):
-    cur = str(channel)
+# improt from 차이
+for length in range(1, 7):
+    for p in product(remain, repeat=length):
+        s_cur = "".join(p) # 000123
 
-    for char in cur:
-        if char in broken:
-            break
-    else:
-        ans = min(ans, len(cur) + abs(channel - n))
+        cur = int(s_cur)
+
+        ans = min(ans, len(str(cur)) + abs(cur - n))
 
 print(ans)
+
+        
+
+
