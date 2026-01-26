d20a4ca7567e2368eec86238b3949a858b2866bf
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2026-01-26 21:56:43 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -9,38 +9,30 @@ vector<int> dp;
 
 
 
-
-int travel(int cur) {
-
-	if (cur > n) return 0;
-
-	if (dp[cur] > 0) return dp[cur];
-
-	int tmp1 = 0, tmp2 = 0;
-	if(cur + times[cur] <= n+1)tmp1 = pays[cur] + travel(cur + times[cur]);
-	tmp2 = travel(cur + 1);
-	dp[cur] = max(tmp1, tmp2);
-	return dp[cur];
-
-}
-
 int main(void) {
 
 	cin >> n;
 	
 	times.resize(n+1);
 	pays.resize(n+1);
-	dp.resize(n+1);
+	dp.resize(n+2);
 
 	for (int i = 1; i < n+1; i++) {
 		cin >> times[i] >> pays[i];
 	}
 
-	int ans = 0;
-	
-	ans = travel(1);
+	for (int i = n; i > 0; i--) {
+		int pass = 0, go = 0;
+		if (i + times[i] <= n + 1) go = pays[i] + dp[i + times[i]];
+		if (i + 1 <= n+1) pass = dp[i + 1];
+		dp[i] = max(go, pass);
+	}
 
-	cout << ans;
+	
+	
+	
+
+	cout << dp[1];
 
 	return 0;
 
