8cd4aa6bef03ee42b6c13265d1bc449b8cb810d3
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2026-02-06 15:35:09 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,67 +1,68 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
-#include<climits>
-#define FAST_IO
+#define FAST_IO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
 
 using namespace std;
 
 int n, m;
-vector<vector<int>> board;
-vector<pair<int, int>> house;
-vector<pair<int, int>> chicken;
-vector<int> comb;
+vector<pair<int, int>> houses;
+vector<pair<int, int>> chickens;
 
+int ans = 0x7fffffff;
+vector<pair<int, int>> picked;
+
+void get_combination(int depth, int idx) {
+
+	// base condiction
+	if (depth == m) {
+		// 각 집에서 치킨거리 구하기!
+		int chicken_sum = 0;
+
+		for (auto h : houses) {
+			// 각 집마다 최솟값 구하기!
+			int mini = 0x7fffffff;
+			for (auto p : picked) {
+				int chicken_dist = abs(h.first - p.first) + abs(h.second - p.second);
+				mini = min(mini, chicken_dist);
+			}
+			chicken_sum += mini;
+		}
+
+		// ans 갱신
+		ans = min(ans, chicken_sum);
+	}
+
+	// index를 돌며 조합 탐색하기!
+	for (int i = idx; i < chickens.size(); i++) {
+		picked.push_back(chickens[i]);
+		get_combination(depth + 1, i + 1);
+		picked.pop_back();
+	}
+}
 
 int main(void) {
-	
+
 	FAST_IO;
 
 	cin >> n >> m;
 
-	board.assign(n, vector<int>(n, 0));
-
+	// 집과 치킨집의 좌푯값 저장!
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 1) house.push_back({ i, j });
-			if (board[i][j] == 2) chicken.push_back({ i, j });
+			int inp;
+			cin >> inp;
+			if (inp == 1) {
+				houses.push_back({ i, j });
+			}
+			else if (inp == 2) {
+				chickens.push_back({ i, j });
+			}
 		}
 	}
 
-
-	comb.assign(chicken.size(), 0);
-	for (int i = 0; i < m; i++) comb[i] = 1;
-	sort(comb.begin(), comb.end());
-
-	int ans = 0x7ffffff;
-
-	do {
-		int sum = 0;
-		for (auto h : house) {
-			int mini = 0x7fffffff;
-			for (int i = 0; i < chicken.size(); i++) {
-				if (comb[i] == 0) continue;
-				pair<int, int> picked = chicken[i];
-				mini = min(mini, abs(h.first - picked.first) + abs(h.second - picked.second));
-			}
-			sum += mini;
-
-		}
-
-		ans = min(ans, sum);
-
-
-		
-
-	} while (next_permutation(comb.begin(), comb.end()));
-	
-
-	
+	get_combination(0, 0);
 
 	cout << ans;
 
 	return 0;
 }
-
-
