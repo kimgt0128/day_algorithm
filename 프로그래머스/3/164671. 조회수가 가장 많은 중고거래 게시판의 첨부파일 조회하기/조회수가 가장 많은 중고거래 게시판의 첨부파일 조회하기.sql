578468892cb4b1b80d69659998675a0a632c3097
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2026-02-19 21:04:09 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,26 @@
+-- 코드를 입력하세요
+WITH MOST_VIEW_BOARD AS (
+    SELECT BOARD_ID
+    
+    FROM USED_GOODS_BOARD
+    
+    ORDER BY VIEWS DESC
+    
+    LIMIT 1
+)
+SELECT 
+    CONCAT(
+        '/home/grep/src/',
+        F.BOARD_ID,
+        '/',
+        F.FILE_ID,
+        F.FILE_NAME,
+        F.FILE_EXT
+    ) AS FILE_PATH
+FROM USED_GOODS_FILE F
+
+JOIN USED_GOODS_BOARD B ON F.BOARD_ID = B.BOARD_ID
+
+WHERE B.BOARD_ID = (SELECT * FROM MOST_VIEW_BOARD)
+
+ORDER BY F.FILE_ID DESC;
