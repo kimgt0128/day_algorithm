e80c47047828bff90a9aef1f97a7d243223be503
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2026-02-24 01:12:07 +0900

    [Bronze III] Title: 네 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(void) {
+	string a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	string s1 = a + b;
+	string s2 = c + d;
+
+	cout << stoll(s1) + stoll(s2);
+}
