b6d4efe511725b383410dccb79cf58bb79446e43
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2026-03-06 22:54:38 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-vector<vector<pair<int, int>>> graph;
-vector<bool> check;
+vector<vector<pair<int, int>>> graph; // {cost, 다음 간선 저장}
+vector<bool> visited;
+
 
 int prime(int start) {
-    
-    int sum = 0;
+    int mst = 0;
+    // 최소 힙!
     priority_queue<
         pair<int, int>,
         vector<pair<int, int>>,
@@ -17,48 +18,48 @@ int prime(int start) {
     pq.push({0, start});
     
     while(!pq.empty()) {
-        
         auto cur = pq.top();
         pq.pop();
-        
         int cur_cost = cur.first;
         int cur_node = cur.second;
         
-        if(check[cur_node]) continue;
-        sum += cur_cost;
-        check[cur_node] = true;
+        if(visited[cur_node] == true) continue;
+        mst += cur_cost;
+        visited[cur_node] = true;
         
         for(auto nxt : graph[cur_node]) {
-            int nxt_node = nxt.second;
             int nxt_cost = nxt.first;
+            int nxt_node = nxt.second;
             
-            if(!check[nxt_node]) {
+            if(visited[nxt_node] == false) {
                 pq.push({nxt_cost, nxt_node});
             }
         }
-        
     }
-    return sum;
-
+    
+    return mst;
+    
 }
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
     
-    graph.resize(n+1);
-    check.resize(n+1, false);
+    // 초기 배열 세팅
+    graph.resize(n);
+    visited.resize(n, false);
     
+    // 양방향 그래프 간선 연결
     for(auto cost : costs) {
-        int u, v, c;
-        u = cost[0];
-        v = cost[1];
-        c = cost[2];
+        int u = cost[0];
+        int v = cost[1];
+        int c = cost[2];
         
         graph[u].push_back({c, v});
         graph[v].push_back({c, u});
     }
     
     answer = prime(0);
+    
     
     
     return answer;
