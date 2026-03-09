bbb09be5b852a3d3dc45a71cc80617a588fb3f81
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2026-03-09 15:38:09 +0900

    [Gold III] Title: 내리막 길, Time: 88 ms, Memory: 4424 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,62 +1,52 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 #define IN(Y, X) (Y >= 0 && Y < n && X >= 0 && X < m)
-#define FAST_IO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
 
 using namespace std;
-
-int dy[4] = { 1, 0, -1, 0 };
-int dx[4] = { 0, 1,0, - 1 };
 
 int n, m;
 vector<vector<int>> board;
 vector<vector<int>> dp;
 
-int dfs(int cy, int cx) {
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
 
-	if (cy == n - 1 && cx == m - 1) return 1;
-
-	int ret = 0;
-
-	for (int i = 0; i < 4; i++) {
-		int ny = cy + dy[i];
-		int nx = cx + dx[i];
-
-		if (IN(ny, nx) && board[ny][nx] < board[cy][cx]) {
-			if (dp[ny][nx] == -1) {
-				dp[ny][nx] = dfs(ny, nx);
-				ret += dp[ny][nx];
-			}
-			else {
-				
-				ret += dp[ny][nx];
-			}
-		}
-	}
-	
-	return dp[cy][cx] = ret;
+int travel(int y, int x) {
+    
+    // base condiction
+    if(y == n - 1 && x == m - 1) return 1;
+    
+    // memoizatoin
+    int& ret = dp[y][x];
+    if(ret != -1) return ret;
+    
+    // state propagation
+    ret = 0;
+    for(int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        // 내리막 칸이고, 범위 안에 있을 때에만!
+        if(IN(ny, nx) && board[ny][nx] < board[y][x]){
+            ret += travel(ny, nx);
+        }
+    }
+    return ret;
 }
 
+
 int main(void) {
-
-	FAST_IO;
-
-	cin >> n >> m;
-
-	board.resize(n, vector<int>(m));
-	dp.resize(n, vector<int>(m, -1));
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> board[i][j];
-		}
-	}
-
-	int ans = dfs(0, 0);
-
-
-	cout << ans;
-	
-
-	return 0;
+    
+    cin >> n >> m;
+    board.resize(n, vector<int>(m, 0));
+    dp.resize(n, vector<int>(m, -1));
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+    
+    int answer = travel(0, 0);
+    cout << answer;
+    
+    return 0;
 }
